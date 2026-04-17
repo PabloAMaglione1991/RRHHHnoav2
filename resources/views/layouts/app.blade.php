d242d126c35fac6a3714c341a6058b18b773d6c3
PabloAMaglione1991 <pablo.maglione1991@gmail.com>
2026-04-17 09:41:16 -0300

    fix: add missing bootstrap js bundle to enable livewire modals

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -141,6 +141,9 @@
         </main>
     </div>
 
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
     @livewireScripts
 </body>
 </html>
